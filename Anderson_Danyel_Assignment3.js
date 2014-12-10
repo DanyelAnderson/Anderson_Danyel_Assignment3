ec049850613aaadfd0ae49478cd817cea5f82789
DanyelAnderson <Danyel@Danyels-MacBook-Pro.local>
2014-12-10 15:42:57 -0500

    First push
    first push

---
+++ Anderson_Danyel_Assignment3.js
@@ -0,0 +1,20 @@
+alert("JavaScript works!");
+
+//Danyel Anderson
+// SDI 1412
+// Assignment 3
+// GamePlay
+
+
+// global variables
+var characterRaces = [ "NightElf", "Tauren", "Human", "Orc", "Goblin" ];
+var characterGender = [ "Male", "Female"];
+var maxLevel = 100;
+
+console.log ("Welcome! Prepare to start your new adventure!");
+
+
+//functions
+var arrayFunction = function (argArray) {
+	console.log ( "You can choose from " + argArray + " to be your race."  );
+}
