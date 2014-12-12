5da5f00fd84db0e600cba01ed4c435ee9737e5fe
DanyelAnderson <Danyel@Danyels-MacBook-Pro.local>
2014-12-11 22:08:06 -0500

    boolean function
    added boolean function

--- Anderson_Danyel_Assignment3.js
+++ Anderson_Danyel_Assignment3.js
@@ -1,4 +1,4 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 
 //Danyel Anderson
 // SDI 1412
@@ -7,29 +7,16 @@ alert("JavaScript works!");
 
 
 // global variables
-var characterRaces = [ "NightElf", "Tauren", "Human", "Orc", "Goblin" ];
+var characterRaces = [  "Tauren", "NightElf", "Orc", "Human", "Goblin", "Gnome" ];
 var characterGender = [ "Male", "Female"];
 var maxLevel = 100;
+var playedBefore = true;
 
+//============================================================
+
+
+//Alert
 console.log ("Welcome! Prepare to start your new adventure!");
-
-
-//functions
-var arrayFunction = function (argArray) {
-	var raceLength = argArray.length;
-	
-	console.log ( "You can choose from " + argArray + " to be your race."  );
-    for ( var i = 0; i <  raceLength; i++ ){
-		
-        }
-}
-
-
-
-
-
-
-
 
 
 //=============================================================
@@ -60,10 +47,119 @@ var jsonData = {
 			]
 	
 }
+console.log(" What class will you choose this time to fight with? ");
+
+var myJSON = function() {
+
+	
+	for (var key in jsonData.classSpecs) {
+		
+		for (var newKey in jsonData.classSpecs[key]) {
+		
+			
+			if (jsonData.classSpecs[key][newKey] == "Paladin") {
+				console.log(" I choose Paladin!");
+				
+				
+				
+			}
+		}
+		
+	}
+	
+	
+	
+};
 
 
+
+
+
+
+
+
+//==============================================================
+
+//functions
+
+var arrayFunction = function (characterRaces) {
+	var raceLength = characterRaces.length;
+	
+	console.log ( "You have " + characterRaces.length + " races to choose from.")
+	
+	console.log ( "You can choose from " + characterRaces + " to be your race."  );
+    for ( var i = 0; i <  raceLength; i++ ){
+		
+        if (characterRaces [i] === "Tauren"){
+			
+			console.log ( characterRaces[i] + " is for the Horde.");
+			
+		
+		} else {
+			
+			console.log ( characterRaces [i++] + " is for the Alliance.");
+			 
+			
+		};
+		
+	};
+
+
+};
+
+
+
+
+// Boolean Function
+
+console.log ("It seems we have and experienced player here.");
+      
+	  var playerSkilled = {
+		  skills : function ( playedBefore ){
+			  if( playedBefore > 1 ){
+				  var skilled = true
+				  return skilled;
+			  }else{
+				  var unSkilled = false
+				  return unskilled;
+			  }
+	   }
+};
+	
+console.log ( " I have played this game it is " + playerSkilled.skills (3) + "  ,and I know my way around a little bit. ")
+
+
+
+
+
+
+//Number Function
+	var startingLevel = 0;
+	var endingLevel = 100;
+	while (startingLevel <= 100 ) {
+		console.log ( startingLevel + " levels down " + endingLevel + " levels until 100.");
+		
+		startingLevel = startingLevel +1;
+		endingLevel = endingLevel -1;
+	   };
+
+console.log (" All levels have been completed at now the real fun can begin. "  );
+			
+			
+
+
+
+
+
+
+
+
+
+//=============================================================
 
 
 //=============================================================	 
 //Main Code
 arrayFunction(characterRaces);
+var myJSON = function() {}
+
